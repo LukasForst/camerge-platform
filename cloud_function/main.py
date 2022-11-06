25e5ac9cea192107649fe465aca80ec53e66ae66
Lukas Forst <lukas@forst.dev>
2022-11-06 23:07:40 +0100

    correctly handle params in cloud function

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -17,9 +17,10 @@ def app(request: Request):
     if request.path.startswith("/version"):
         return {'message': 'hello world'}
     # calendar endpoint
-    if request.path.find('/calendar/') != -1:
-        key = request.path.lstrip('/calendar/')
+    maybe_calendar_param = request.path.split('/calendar/', 1)
+    if len(maybe_calendar_param) > 1:
+        key = maybe_calendar_param[1]
         ical, error = service.get_for_key(key)
-        return Response(ical, mimetype='text/calendar')
+        return Response(ical, mimetype='text/calendar') if ical else Response(error, status=404)
     # not found
     return {'message': 'Not Found.'}, 404
