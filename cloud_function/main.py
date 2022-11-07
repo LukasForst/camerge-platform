646abb38f5b6921fa5afee8cd925aced903eb05c
Lukas Forst <lukas@forst.dev>
2022-11-07 11:53:58 +0100

    fix 404 even on success

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -21,6 +21,6 @@ def app(request: Request):
     if len(maybe_calendar_param) > 1:
         key = maybe_calendar_param[1]
         ical, error = service.get_for_key(key)
-        return Response(ical, mimetype='text/calendar') if ical else {'error': error}, 404
+        return Response(ical, mimetype='text/calendar') if ical else ({'error': error}, 404)
     # not found
     return {'message': 'Not Found.'}, 404
